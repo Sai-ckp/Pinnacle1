efe7517ea515ada0c633e9004b46ccfb5f4414a3
Sai-ckp <saishashank@ckpsoftware.com>
2025-05-30 18:21:19 +0530

    Update urls.py

--- student_alerts_app/urls.py
+++ student_alerts_app/urls.py
@@ -2,8 +2,11 @@
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from master.views import dashboard_view
+
 
 urlpatterns = [
+    path('', dashboard_view, name='home'),
     path('admin/', admin.site.urls),
     path('master/', include('master.urls')),         # Previously '', now 'master/'
     path('admission/', include('admission.urls')),   # Previously '', now 'admission/'
