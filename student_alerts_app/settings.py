8acadae80785ab2ef766bc4926b231d5c4b5d554
Sai-ckp <saishashank@ckpsoftware.com>
2025-05-30 13:21:46 +0530

    Update settings.py

--- student_alerts_app/settings.py
+++ student_alerts_app/settings.py
@@ -129,10 +129,11 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
 LANGUAGE_CODE = 'en-us'
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Kolkata'
+USE_TZ = True
+
 USE_I18N = True
 USE_L10N = True
-USE_TZ = True
 # settings.py
 
 LOGIN_REDIRECT_URL = '/dashboard/'
